5acc5eb71c75618f9da34557fcfd0bec88da65b8
Bahtiar <bahtiar26@gmail.com>
2021-05-25 17:47:21 +0600

    delete .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path, os
 from django.conf.urls.static import static
-import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'e*2gzq&fvv07zjp-3vs#y9v7y+85$lchm41vg4e_0%im%ja4z2'
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env()
-DEBUG = env('DEBUG')
-SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
